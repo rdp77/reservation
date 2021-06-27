fb5355b458d993b4221886919c26036a395aab5e
RDP77 <ravidwiputra@gmail.com>
2021-06-27 21:31:45 +0700

    Update Pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('titleContent', __('auth.login'))
 
 @section('content')
-<form method="POST" action="#" class="needs-validation" novalidate="">
+<form method="POST" action="#" class="needs-validation">
     <div class="form-group">
         <label>{{ __('Tanggal') }}</label>
         <div class="input-group">
