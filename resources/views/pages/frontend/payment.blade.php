717fa60d40d1fb69159a674a956f0d0cb9377d2d
RDP77 <ravidwiputra@gmail.com>
2021-06-29 02:45:15 +0700

    Update Page Payment

--- resources/views/pages/frontend/payment.blade.php
+++ resources/views/pages/frontend/payment.blade.php
@@ -8,9 +8,10 @@
     <h2><mark>{{ __('Rp. 500.000.258') }}</mark></h2>
     <p class="lead">
         {{ __('Lakukan pembayaran sejumlah nominal yang sudah 
-        ditentukan diatas, dengan metode pembayaran melalui BCA. Konfirmasi 
-        pembayaran akan kami kirimkan via whatsapp, sms, ataupun email yang 
-        sudah tertaut sebelumnya') }}
+        ditentukan diatas, dengan menggunakan metode pembayaran bank BCA atas nama') }}
+        <u>{{ __('Talentitya Ryzabel.') }}</u>
+        {{ __(' Konfirmasi pembayaran akan kami kirimkan via whatsapp, sms, 
+        ataupun email yang sudah tertaut sebelumnya.') }}
     </p>
     <a href="#" class="mt-4 bb">
         {{ __('Pembayaran masih belum terkonfirmasi? Hubungi CS kami') }}
